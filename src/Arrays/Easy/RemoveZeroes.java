03c4bca86db3815eb6826f5ac2ddac41b2fcdf81
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-02-20 19:38:51 +0530

    RemoveZeroes modified

--- src/Arrays/Easy/RemoveZeroes.java
+++ src/Arrays/Easy/RemoveZeroes.java
@@ -12,32 +12,60 @@ public class RemoveZeroes {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-
-        if(nums.length == 1 || (nums.length==2 && nums[0]!=0))
-            return;
-
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0 && nums[j] == 0)
-                continue;
-            else if(nums[i] != 0 &&  nums[j] == 0)
-                j=swap(nums,i,j);
-            else if(nums[i] != 0 && nums[j] !=0)
-            {
-                j++;
-            }
-        }
+//        RemoveZero1(nums);
+        RemoveZero2(nums);
         System.out.println(Arrays.toString(nums));
     }
+        public static void RemoveZero1 (int nums[]){
+            if(nums.length == 1 || (nums.length==2 && nums[0]!=0))
+                return;
+            int j=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i] == 0 && nums[j] == 0)
+                    continue;
+                else if(nums[i] != 0 &&  nums[j] == 0)
+                    j=swap(nums,i,j);
+                else if(nums[i] != 0 && nums[j] !=0)
+                {
+                    j++;
+                }
+            }
 
-    public static int swap(int[] arr, int i, int j){
+    }
+
+    public static void RemoveZero2(int[] nums){
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
+                j=i;
+                break;
+            }
+        }
+
+        if (j==-1)
+            return;
+
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0)
+            {   swap2(nums,i,j);
+            j++;}
+        }
+    }
+
+    public static void swap2(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-        if(arr[j+1] == 0)
-            j=j+1;
-        else
-            j=i;
-        return j;
+
+    }
+
+    public static int swap(int[] arr, int i, int j){
+
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        return j+1;
     }
 }
