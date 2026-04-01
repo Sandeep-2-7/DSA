45aec2d7e5150786084a97ef89a4e3dfbb383fe5
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-04-01 21:25:03 +0530

    sort 0 1 and 2s modified

--- src/Arrays/Medium/Sort_0_1_and_2s.java
+++ src/Arrays/Medium/Sort_0_1_and_2s.java
@@ -42,4 +42,31 @@ public class Sort_0_1_and_2s {
             arr[i+count0+count1] = 2;
         }
     }
+
+    public static void sort012sV2(int[] arr){
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] == 0)
+                count0++;
+            else if (arr[i] == 1)
+                count1++;
+            else if (arr[i] == 2)
+                count2++;
+        }
+
+        for(int i=0;i< count0;i++){
+            arr[i] = 0;
+        }
+
+        for(int i=0; i<count1; i++){
+            arr[i+count0] = 1;
+        }
+
+        for(int i=0;i<count2; i++){
+            System.out.println();
+            arr[i+count0+count1] = 2;
+        }
+    }
 }
