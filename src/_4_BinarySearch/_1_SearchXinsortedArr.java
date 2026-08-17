7f03a136a60bc9aaca3a0128fe5da0ed1cad32d3
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-08-17 16:02:26 +0530

    Binary Search

--- src/_4_BinarySearch/_1_SearchXinsortedArr.java
+++ src/_4_BinarySearch/_1_SearchXinsortedArr.java
@@ -8,14 +8,32 @@ public class _1_SearchXinsortedArr {
         System.out.println("Enter the length of the array : ");
         int n = sc.nextInt();
 
+        System.out.println("Enter the target");
+        int target = sc.nextInt();
+
         System.out.println("Enter the elements of the array : ");
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
+        System.out.println(BS(nums, target));
     }
 
-//    public static int BS(int[] arr){
-//
-//    }
+    public static int BS(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = (start-end)/2 + end;
+
+            if(arr[mid] == target){
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid+1;
+            } else if (arr[mid] > target) {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
 }
