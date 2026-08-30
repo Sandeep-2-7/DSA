aaaa24e7a7e3d1a6a15d42624533fb73201c7de4
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-08-30 19:53:58 +0530

    Search In Rotated Arr

--- src/_4_BinarySearch/_8_SearchInRotatedarr.java
+++ src/_4_BinarySearch/_8_SearchInRotatedarr.java
@@ -7,28 +7,47 @@ public class _8_SearchInRotatedarr {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array : ");
         int n = sc.nextInt();
-
+        System.out.println("Enter the target");
+        int target = sc.nextInt();
         System.out.println("Enter the elements of the array : ");
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-        System.out.println(pivot(nums));
-
+        int pivot = pivot(nums);
+        int ans = -1;
+        if(nums[pivot] == target)
+            ans = pivot;
+        else if(pivot-1>=0 && nums[0] <= target && target <= nums[pivot-1])
+            ans = BS(nums, 0, pivot-1, target);
+        else if(nums[pivot] <= target && target <= nums[nums.length-1])
+            ans = BS(nums, pivot, nums.length-1, target);
+        System.out.println(ans);
     }
+    public static int BS(int[] arr, int start, int end, int target){
+        while(start <= end){
+            int mid = (end-start)/2 + start;
 
+            if(arr[mid] > target)
+                end = mid-1;
+            else if(arr[mid] < target)
+                start = mid+1;
+            else
+                return mid;
+        }
+        return -1;
+    }
     public static int pivot(int[] arr){
         int start = 0;
         int end = arr.length-1;
-        int ans = -1;
 
         while(start < end){
-            int mid = (end-start)/2 + start;
+            int mid = (end - start)/2 + start;
 
             if(arr[mid] > arr[end])
-                start = mid+1;
+                start=mid+1;
             else
-                end = mid;
+                end=mid;
         }
         return start;
     }
