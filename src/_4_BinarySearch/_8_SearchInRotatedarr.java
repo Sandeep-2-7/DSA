561ad8317806ccd9dbee85863de9fd86b06ac5d8
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-08-30 19:51:10 +0530

    Search In Rotated Arr

---
+++ src/_4_BinarySearch/_8_SearchInRotatedarr.java
@@ -0,0 +1,35 @@
+package _4_BinarySearch;
+
+import java.util.Scanner;
+
+public class _8_SearchInRotatedarr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the array : ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements of the array : ");
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(pivot(nums));
+
+    }
+
+    public static int pivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+
+        while(start < end){
+            int mid = (end-start)/2 + start;
+
+            if(arr[mid] > arr[end])
+                start = mid+1;
+            else
+                end = mid;
+        }
+        return start;
+    }
+}
