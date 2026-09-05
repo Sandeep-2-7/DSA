3e5db97a4776267a0ea701ce642e084846977a38
Sandeep-2-7 <sandeep27102010@gmail.com>
2026-09-05 10:40:32 +0530

    Search In Rotated Arr

--- src/_4_BinarySearch/_8_SearchInRotatedarr.java
+++ src/_4_BinarySearch/_8_SearchInRotatedarr.java
@@ -14,16 +14,18 @@ public class _8_SearchInRotatedarr {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-        int pivot = pivot(nums);
-        int ans = -1;
-        if(nums[pivot] == target)
-            ans = pivot;
-        else if(pivot-1>=0 && nums[0] <= target && target <= nums[pivot-1])
-            ans = BS(nums, 0, pivot-1, target);
-        else if(nums[pivot] <= target && target <= nums[nums.length-1])
-            ans = BS(nums, pivot, nums.length-1, target);
-        System.out.println(ans);
+        System.out.println(searching(nums, target));
+
+//        int ans = -1;
+//        if(nums[pivot] == target)
+//            ans = pivot;
+//        else if(pivot-1>=0 && nums[0] <= target && target <= nums[pivot-1])
+//            ans = BS(nums, 0, pivot-1, target);
+//        else if(nums[pivot] <= target && target <= nums[nums.length-1])
+//            ans = BS(nums, pivot, nums.length-1, target);
+//        System.out.println(ans);
     }
+
     public static int BS(int[] arr, int start, int end, int target){
         while(start <= end){
             int mid = (end-start)/2 + start;
@@ -50,5 +52,34 @@ public class _8_SearchInRotatedarr {
                 end=mid;
         }
         return start;
+    }
+
+    public static int searching(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = (end - start)/2 + start;
+
+            if(arr[mid] == target)
+                return mid;
+
+            if(arr[mid] >= arr[start]){
+                if(arr[start] <= target && target <= arr[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+            else if(arr[mid] <= arr[end]){
+                if(arr[mid] <= target && target <= arr[end])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+
+
+        }
+
+        return -1;
     }
 }
